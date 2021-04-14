ca7b4800c93523d7b3ede13d6186b99ba66c8ac0
Keller Pascal <pascalkeller99@gmail.com>
2021-04-14 19:11:10 +0200

    projet fonctionnel et prêt à partir en production

---
+++ README.md
@@ -0,0 +1,33 @@
+# MyBanque
+Projet ECF Banque
+
+installer toutes les dépendances
+
+Creer la base de donnees mybanque
+
+migrer les tables
+
+Les 5 comptes banquiers sont automatiquement creer lors de la migration des tables
+
+Voici les identifiants :
+
+Banqiuer 1 :
+Email : banquier1@symfony.fr
+Mdp :   banquier1
+
+Banqiuer 2 :
+Email : banquier2@symfony.fr
+Mdp :   banquier2
+
+Banqiuer 3 :
+Email : banquier3@symfony.fr
+Mdp :   banquier3
+
+Banqiuer 4 :
+Email : banquier4@symfony.fr
+Mdp :   banquier4
+
+Banqiuer 5 :
+Email : banquier5@symfony.fr
+Mdp :   banquier5
+
--- migrations/Version20210409144345.php
+++ migrations/Version20210409144345.php
@@ -28,8 +28,23 @@ final class Version20210409144345 extends AbstractMigration
 
         $this->addSql('INSERT INTO roles (role_name) VALUES ("ROLE_USER")');
         $this->addSql('INSERT INTO roles (role_name) VALUES ("ROLE_ADMIN")');
+
         $this->addSql('INSERT INTO user (firstname, lastname, mail, password, identity_file, validation) 
-                                    VALUES ("admin", "admin", "admin@symfony.fr", "$2a$12$LRYlS6grzmtOycCLzA7sEu5cGdVG8FrcynP5Nu0NgXGQVeZEE44Im", "nothing", 1)');
+        VALUES ("Banquier1", "Banquier1", "banquier1@symfony.fr", "$2y$12$CWHHp8Bi3.Y67tbhAbkBZeAmP5vZCym4yNwSXp2QlrzuVYDUniNCK", "nothing", 1)');
+
+        $this->addSql('INSERT INTO user (firstname, lastname, mail, password, identity_file, validation) 
+        VALUES ("Banquier2", "Banquier2", "banquier2@symfony.fr", "$2y$12$GZNZ4Uymmv5FIQba9zfg8etA93SdMafXjfccSlRhmS51XNdrKS0uu", "nothing", 1)');
+
+        $this->addSql('INSERT INTO user (firstname, lastname, mail, password, identity_file, validation) 
+        VALUES ("Banquier3", "Banquier3", "banquier3@symfony.fr", "$2y$12$YYrBfGl66Ic5/rfuikmR6.3/h0npaxvrccvuDKkL84AO.H4VPteI", "nothing", 1)');
+
+        $this->addSql('INSERT INTO user (firstname, lastname, mail, password, identity_file, validation) 
+        VALUES ("Banquier4", "Banquier4", "banquier4@symfony.fr", "$2y$12$lBjxgx6m6aHn.pspHkMBKOIjWXNAvlk7JO45m9Ao21DrenRm1l776", "nothing", 1)');
+
+        $this->addSql('INSERT INTO user (firstname, lastname, mail, password, identity_file, validation) 
+        VALUES ("Banquier5", "Banquier5", "banquier5@symfony.fr", "$2y$12$Fj3IZn8jtLRf/lkXJTMYdezdT58QbdjtjPlUrjskDoV3PDcZTFwaO", "nothing", 1)');
+
+
         $this->addSql('INSERT INTO roles_user (roles_id, user_id) VALUES (2, 1)');
     }
 
